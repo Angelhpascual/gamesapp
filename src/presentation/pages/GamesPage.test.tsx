76e00b353d458971880183f3cdc5124fceacb9bb
Angelhpascual <Angelhpascual@gmail.com>
2025-10-06 11:39:00 +0200

    feat: enhance GamesPage tests with error handling and empty state scenarios

--- src/presentation/pages/GamesPage.test.tsx
+++ src/presentation/pages/GamesPage.test.tsx
@@ -2,6 +2,7 @@ import { afterEach, describe, expect, it } from "vitest"
 import { mswServer } from "../../../vitest.setup"
 import { cleanup, render, waitFor, screen } from "@testing-library/react"
 import GamesPage from "./GamesPage"
+import { http, HttpResponse } from "msw"
 
 describe("GamesPage", () => {
   afterEach(() => {
@@ -13,6 +14,27 @@ describe("GamesPage", () => {
     render(<GamesPage />)
     await waitFor(() =>
       expect(screen.getByText("Mock Game")).toBeInTheDocument()
+    )
+  })
+
+  it("Muestra un mensaje de error cuando la API responde 500", async () => {
+    mswServer.use(
+      http.get("*/games", () =>
+        HttpResponse.json({ message: "Internal Server Error" }, { status: 500 })
+      )
+    )
+    render(<GamesPage />)
+    await waitFor(() =>
+      expect(
+        screen.getByText(/Error al cargar los juegos/i)
+      ).toBeInTheDocument()
+    )
+  })
+  it("Muestra un mensaje de ausencia de datos cuando la lista está vacía", async () => {
+    mswServer.use(http.get("*/games", () => HttpResponse.json([])))
+    render(<GamesPage />)
+    await waitFor(() =>
+      expect(screen.getByText(/no hay juegos disponibles/i)).toBeInTheDocument()
     )
   })
 })
